f39c0319cf8164ad3906dacdfefc6d6b1efc23fb
emmanuelguty <manuel15901@gmail.com>
2022-12-28 23:25:20 -0600

    Se termino de agregar todos los archivos necesarios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+//Se importaran todos los componentes
+import { Navbar, Brand, CTA } from './components';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from '/containers'; 
+
 
 
 const App = () => {
