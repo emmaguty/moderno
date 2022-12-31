bbcbfc35beb16c38d8ae00eee17a301e2327e93a
emmanuelguty <manuel15901@gmail.com>
2022-12-31 17:02:50 -0600

    Se agregaron los assets que se van a usar en el  proyecto

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from 'react'; 
 import './navbar.css';
+//Icons
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+
+//BEM -> Block Element Modified
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links-logo'>
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
   )
 }
 
