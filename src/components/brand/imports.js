eab159d5388c6ad4ec4d2bb23ca5d2557965fec3
emmanuelguty <manuel15901@gmail.com>
2023-01-02 17:05:43 -0600

    Se termino el contenedor brand con sus estilos correspondientes

---
+++ src/components/brand/imports.js
@@ -0,0 +1,14 @@
+// Este archivo me servira para importar todas las imagenes de una forma mas limpia
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
+
+export {
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify
+}
