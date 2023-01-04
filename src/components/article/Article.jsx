047e8948280abac11b7bee97db53fc61d6612610
emmanuelguty <manuel15901@gmail.com>
2023-01-03 23:30:25 -0600

    Se uso los props de las imagenes

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './article.css';
 
-const Article = () => {
+// Cambiar la imagen para todos los 
+//Para eso se usara los props
+const Article = ({ imgUrl }) => {
   return (
-    <div>Article</div>
+    <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container_article-image'>
+        <img src={imgUrl} alt="blog image" />
+      </div>
+    </div>
   )
 }
 
