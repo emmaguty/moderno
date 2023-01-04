5f38077c0549745b0d3f30614ccef8100755ed57
emmanuelguty <manuel15901@gmail.com>
2023-01-04 16:53:59 -0600

    Se termino el componente de Articulo

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -11,7 +11,7 @@ const Blog = () => {
       </div>
       <div className='gpt3__blog-container'>
         <div className='gpt3__blog-container_groupA'>
-          <Article imgUrl={blog01} date="Sep 26, 2022" />
+          <Article imgUrl={blog01} date="Sep 26, 2022" title="GPT-3 and Open AI is the future. Let us explore how it is?"/>
         </div>
         <div className='gpt3__blog-container_groupB'>
           <Article imgUrl={blog02} date="Sep 26, 2022" title="GPT-3 and Open AI is the future. Let us explore how it is?"/>
