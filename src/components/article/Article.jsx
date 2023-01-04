30e410a025582bc1585dbd0d0cdbb1f2ad676451
emmanuelguty <manuel15901@gmail.com>
2023-01-03 23:56:35 -0600

    Se corrigio el tamaño de la imagen del contenedor possibility

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -9,6 +9,13 @@ const Article = ({ imgUrl, date, title}) => {
       <div className='gpt3__blog-container_article-image'>
         <img src={imgUrl} alt="blog"/>
       </div>
+      <div className='gpt3__blog-container_article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+          <p>Read Full Article</p>
+      </div>
     </div>
   )
 }
